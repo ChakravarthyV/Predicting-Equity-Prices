7dd34602f763b822374617dddc3a93bddfda691c
Chakravarthy V <chakravarthyv997@gmail.com>
2021-11-16 19:10:30 +0000

    added stock prices column (in sync w/ respective quarter)

--- batch_learning.py
+++ batch_learning.py
@@ -2,38 +2,48 @@ import requests
 import json
 import pandas as pd
 import numpy as np
+import yfinance as yf
+import datetime
 
-"""
-Given the API's daily limit to the number of calls allowed, the purpose of this file is to 
-test and train our model offline. It is assumed that you have the company's json-formatted balance sheet, 
-income statement, and cash flow statement downloaded locally.
-"""
+#function to find nearest date
+def nearest(items, pivot):
+    return min(items, key=lambda x: abs(x - pivot))
 
-#Locate offline accounting statements, assumes for now that the company is IBM
-b_s = json.load(open('balance_sheet.json',))
-i_s = json.load(open('income_statement.json',))
-c_f = json.load(open('income_statement.json',))
+#offline training
+price_df = yf.download("IBM", start="2014-01-01", end="2021-11-1") #YYYY-MM-DD
+price_df = price_df.reset_index() #set 'Date' as column
+price_df.to_csv('uhhh.csv', encoding='utf-8', index=False)
 
-#Convert json files into dataframes -- break them up into quarterly reports
-bs_df = pd.DataFrame(b_s['quarterlyReports'])
-is_df =  pd.DataFrame(i_s['quarterlyReports'])
-cf_df = pd.DataFrame(c_f['quarterlyReports'])
+#sample case: IBM
+bs_json = json.load(open('balance_sheet.json',))
+is_json = json.load(open('income_statement.json',))
+cf_json = json.load(open('income_statement.json',))
 
-
-df1 = pd.merge(is_df, cf_df, left_index=True, right_index=True, how='outer', suffixes=('', '_y')) #due to repeated columns in CF and IS, keep 2nd copies of repeating columns and non-repeating columns only
-df1.drop(df1.filter(regex='_y$').columns.tolist(),axis=1, inplace=True) #now, remove portion of column names that show that it's a 2nd copy
+#convert json files into dataframes -- break them up into quarterly reports
+bs_df = pd.DataFrame(bs_json['quarterlyReports'])
+is_df =  pd.DataFrame(is_json['quarterlyReports'])
+cf_df = pd.DataFrame(cf_json['quarterlyReports'])
+df1 = pd.merge(is_df, cf_df, left_index=True, right_index=True, how='outer', suffixes=('', '_y')) #keep 2nd copies of repeating columns only
+df1.drop(df1.filter(regex='_y$').columns.tolist(),axis=1, inplace=True) #remove portion of column names that show that it's a 2nd copy
 
 #Company's quarterly reports collected and cleaned from all three accounting statements.
-ibm_df = pd.merge(bs_df, df1, on=['fiscalDateEnding','reportedCurrency']) #merge BS with the cleaned up combination of CF and IS on the only two shared columns they have
+refined_df = pd.merge(bs_df, df1, on=['fiscalDateEnding','reportedCurrency']) #merge BS with the cleaned up combination of CF and IS on the only two shared columns they have
 
 #Loop through each column and replace any 'None' strings with the value NaN
-for col in ibm_df.columns:
-    ibm_df[col].replace('None', np.nan, inplace=True)
+for col in refined_df.columns:
+    refined_df[col].replace('None', np.nan, inplace=True)
+refined_df = refined_df.fillna(0) #fill up all NaN values with the integer 0
+refined_df['fiscalDateEnding'] =  pd.to_datetime(refined_df['fiscalDateEnding']) #convert date column to datetime
 
-ibm_df = ibm_df.fillna(0) #fill up all NaN values with the integer 0
+dates_list = []
+for quarter_date in refined_df['fiscalDateEnding']:
+    dates_list.append(nearest(price_df['Date'].to_list(), quarter_date)) #find dates in yfinance data closest matching those of quarterly filings
 
-#Lo and behold, a company's quarterly filings merged across all three accounting statements, represented as a clean dataframe
-print(ibm_df)
+prices_list = []
+for date in dates_list:
+    prices_list.append(price_df.loc[price_df['Date'] == date, 'Close'].item()) #for those dates, get closing day stock prices
+
+refined_df['stockPrice'] = prices_list #set stock prices as new df
 
 #Export dataframe into a csv (check it out yourself)
-ibm_df.to_csv('sample.csv', encoding='utf-8', index=False)
+refined_df.to_csv('sample.csv', encoding='utf-8', index=False)
