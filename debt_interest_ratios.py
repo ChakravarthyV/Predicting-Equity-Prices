4126d540f72da5c6ad1d7f66b2939e7b1d996ba7
Chakravarthy V <chakravarthyv997@gmail.com>
2021-11-16 13:59:00 +0000

    minor edits in calculations

--- debt_interest_ratios.py
+++ debt_interest_ratios.py
@@ -1,19 +1,16 @@
 import pandas as pd
-import numpy as np
 #Debt and Interest ratios
 
 #Importing the csv file
 df = pd.read_csv (r'sample.csv')
-#create new dataframe with columns for each ratio
-df2 = pd.DataFrame(columns=['interestCoverage', 'debtToCap', 'totalDebt'])
+#create empty dataframe
+df2 = pd.DataFrame()
 
 #Calculate each ratio and append to the respective column in the dataframe
-df2['interestCoverage'] = df['ebit'] / df['interestExpense']
-df2['debtToCap'] = df['longTermDebt'] / df['totalShareholderEquity']
-df2['totalDebt'] = (df['shortTermDebt'] + df['longTermDebt']) / df['totalAssets']
+df2['interestCoverage'] = df['interestExpense']/df['ebit']
+df2['debtToCap'] = df['longTermDebt']/(df['totalShareholderEquity'] + df['longTermDebt'])
+df2['debtToAssets'] = (df['shortTermDebt'] + df['longTermDebt']) / df['totalAssets']
 
-#Start index from 1
-df2.index = np.arange(1, len(df2)+1)
 #Export dataframe into a csv 
 df2.to_csv('debt_interest.csv', encoding='utf-8', index=False)
-print (df2)
+print(df2)
--- profit_ratios.py
+++ profit_ratios.py
@@ -1,23 +1,20 @@
 import pandas as pd
-import numpy as np
 #Profitability ratios
 
 #Importing the csv file 
 df = pd.read_csv (r'sample.csv')
-#create new dataframe with columns for each ratio
-df1 = pd.DataFrame(columns=['grossMargin', 'operProfitMargin', 'pretaxProfitMargin', 'netProfitMargin', 'returnInvestedCapital', 'returnOnEquity', 'returnOnAssets'])
+#create empty dataframe
+df1 = pd.DataFrame()
 
 #Calculate each ratio and append to the respective column in the dataframe
-df1['grossMargin'] = df['grossProfit'] / df['totalRevenue']
-df1['operProfitMargin'] = df['ebitda'] / df['totalRevenue']
-df1['pretaxProfitMargin'] = df['incomeBeforeTax']
-df1['netProfitMargin'] = df['netIncome'] - df['incomeTaxExpense']
-df1['returnInvestedCapital'] = (df['shortTermDebt'] + df['longTermDebt']) / df['totalShareholderEquity']
-df1['returnOnEquity'] = df['netIncome'] / df['totalShareholderEquity']
-df1['returnOnAssets'] = df['netIncome'] / df['totalAssets']
+df1['grossMargin'] = df['grossProfit']/df['totalRevenue']
+df1['operProfitMargin'] = df['ebitda']/df['totalRevenue']
+df1['pretaxProfitMargin'] = df['incomeBeforeTax']/df['totalRevenue']
+df1['netProfitMargin'] = df['netIncome']/df['totalRevenue']
+df1['returnInvestedCapital'] = df['netIncome']/(df['shortTermDebt']+df['longTermDebt']+df['totalShareholderEquity'])
+df1['returnOnEquity'] = df['netIncome']/df['totalShareholderEquity']
+df1['returnOnAssets'] = df['netIncome']/df['totalAssets']
 
-#Start index from 1
-df1.index = np.arange(1, len(df1)+1)
 #Export dataframe into a csv 
 df1.to_csv('profit_ratios.csv', encoding='utf-8', index=False)
 print (df1)
